c5b77df24012ee49dbfea97bff21c93dd8f01134
Alik X <scripdoc@gmail.com>
2019-03-07 00:57:11 +0300

    Added task #1

---
+++ task-1/users-data.js
@@ -0,0 +1,31 @@
+var usersData = [{
+    "id": 1,
+    "name": "Эльвира",
+    "surname": "Сорокина",
+    "patronimic": "Аркадьевна",
+    "icon_url": "http://randomuser.ru/images/women/med/70.jpg"
+}, {
+    "id": 2,
+    "name": "Владимир",
+    "surname": "Сидоров",
+    "patronimic": "Вячеславович",
+    "icon_url": "http://randomuser.ru/images/men/med/40.jpg"
+}, {
+    "id": 3,
+    "name": "Василий",
+    "surname": "Блинов",
+    "patronimic": "Денисович",
+    "icon_url": "http://randomuser.ru/images/men/med/34.jpg"
+}, {
+    "id": 4,
+    "name": "Анатолий",
+    "surname": "Тихонов",
+    "patronimic": "Викторович",
+    // "icon_url": "http://randomuser.ru/images/men/med/12.jpg"
+}, {
+    "id": 5,
+    "name": "Кира",
+    "surname": "Исаева",
+    "patronimic": "Михайловна",
+    "icon_url": "http://randomuser.ru/images/women/med/61.jpg"
+}];
