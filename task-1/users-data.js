5720e1feaf4f8786241b53f8d37d82059ed24292
Alik X <scripdoc@gmail.com>
2019-03-07 16:57:24 +0300

    Fixed task #1 (code review)

--- task-1/users-data.js
+++ task-1/users-data.js
@@ -2,30 +2,30 @@ var usersData = [{
     "id": 1,
     "name": "Эльвира",
     "surname": "Сорокина",
-    "patronimic": "Аркадьевна",
+    "patronymic": "Аркадьевна",
     "icon_url": "http://randomuser.ru/images/women/med/70.jpg"
 }, {
     "id": 2,
     "name": "Владимир",
     "surname": "Сидоров",
-    "patronimic": "Вячеславович",
+    "patronymic": "Вячеславович",
     "icon_url": "http://randomuser.ru/images/men/med/40.jpg"
 }, {
     "id": 3,
     "name": "Василий",
     "surname": "Блинов",
-    "patronimic": "Денисович",
+    "patronymic": "Денисович",
     "icon_url": "http://randomuser.ru/images/men/med/34.jpg"
 }, {
     "id": 4,
     "name": "Анатолий",
     "surname": "Тихонов",
-    "patronimic": "Викторович",
+    "patronymic": "Викторович",
     "icon_url": "" // http://randomuser.ru/images/men/med/12.jpg
 }, {
     "id": 5,
     "name": "Кира",
     "surname": "Исаева",
-    "patronimic": "Михайловна",
+    "patronymic": "Михайловна",
     "icon_url": "http://randomuser.ru/images/women/med/61.jpg"
 }];
